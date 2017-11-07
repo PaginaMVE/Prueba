99113fad8969883cedaaeca4daca2657b944323a
Kevin <kevinsl@live.com.mx>
2017-11-07 13:04:24 -0600

    Primera vez

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function validar(){
+	var reemplazo = document.getElementById("reemplazar");
+	reemplazo.innerHTML = "<div class='alert alert-success'><strong>Success!</strong> Indicates a successful or positive action.</div>"
+}
